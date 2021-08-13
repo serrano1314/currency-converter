bf78dbbeeff8490abe41bb8ca6f7d7c4b664e4b6
Steven Serrano <steven.serrano@tup.edu.ph>
2021-08-13 14:09:36 +0800

    add starting test API

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let price_input_text = document.getElementById('price-input');
+let conv_price = document.getElementById('converted-price');
+let price_change = document.getElementById('prc-change')
+let coin = "bitcoin";
+let currency = "php";
+price_input_text.addEventListener('keyup',getCoinValue)
+getCoinValue()
+function getCoinValue(){
+    let price_input = document.getElementById('price-input').value;
+    fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${currency}&include_24hr_change=true`)
+    .then((res)=>res.json())    
+    .then((data)=>{
+        console.log(data);
+        conv_price.value = (data[coin][currency])*price_input;
+        price_change.innerText = "Price Change(24h): " + data[coin][`${currency}_24h_change`].toFixed(2);
+        
+    })
+}
