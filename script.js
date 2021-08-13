050ba39f49dc441f434fc1f3576bee7b92a251e6
Steven Serrano <steven.serrano@tup.edu.ph>
2021-08-13 14:36:12 +0800

    add some coin in the list

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 
 let price_input_text = document.getElementById('price-input');
 let conv_price = document.getElementById('converted-price');
-let price_change = document.getElementById('prc-change')
-let coin = "bitcoin";
-let currency = "php";
+let price_change = document.getElementById('prc-change');
+let selected_coin = document.getElementById('seleted-coin');
+let selected_currency = document.getElementById('selected-currency');
+
 price_input_text.addEventListener('keyup',getCoinValue)
+selected_coin.addEventListener('change',getCoinValue)
+selected_currency.addEventListener('change',getCoinValue)
 getCoinValue()
 function getCoinValue(){
+    let coin = selected_coin.value;
+    let currency = selected_currency.value;
     let price_input = document.getElementById('price-input').value;
     fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${currency}&include_24hr_change=true`)
     .then((res)=>res.json())    
