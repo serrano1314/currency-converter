8b6b38ed4e72668098bf42610a3f9a1f2985641f
Steven Serrano <steven.serrano@tup.edu.ph>
2021-08-14 18:19:10 +0800

    add chart

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ let selected_coin = document.getElementById('seleted-coin');
 let selected_currency = document.getElementById('selected-currency');
 let refres_icon = document.getElementById('refresh-icon');
 let refresh_but = document.getElementById('refresh');
-let coin_title = document.getElementById('coin-title')
+let coin_title = document.getElementById('coin-title');
+let chart_container = document.getElementById('weekly-chart');
+let ctx = document.getElementById('weekly-chart').getContext('2d');
+let myChart;
 
 price_input_text.addEventListener('change',getCoinValue)
 price_input_text.addEventListener('keyup',getCoinValue)
@@ -24,7 +27,6 @@ function getCoinValue(){
     fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${currency}&include_24hr_change=true`)
     .then((res)=>res.json())    
     .then((data)=>{
-        stopLoading()
         console.log(data)
         coin_title.innerText = selected_coin.value.charAt(0).toUpperCase() + selected_coin.value.slice(1);
         conv_price.value = ((data[coin][currency])*price_input);
@@ -35,8 +37,43 @@ function getCoinValue(){
         price_change.innerText = prc_change_value.toFixed(2) + "%";
         price_change.style.color = percentage_color;
     })
+
+    //chart
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=${currency}&days=7&interval=daily`)
+    .then((res)=>res.json())    
+    .then((dataPrice)=>{
+        console.log(dataPrice.prices)
+        stopLoading()
+        Chart.defaults.scale.ticks.display = false;
+        myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['','','','','','','','',],
+                datasets: [{
+                    data: dataPrice.prices,
+                    borderColor: 'rgb(255, 75, 75)',
+                    fill: false,
+                }],
+            },
+            options: {
+                plugins:{
+                    legend:{display:false}
+                },
+                scales: {
+                    x: {
+                    grid: {display: false,drawBorder: false,}
+                    },
+                    y: {
+                    grid: {display: false,drawBorder: false,}
+                    },
+                },
+            }
+        });
+    })
+    myChart.destroy();
 }
-setInterval(getCoinValue,10000)
+setInterval(getCoinValue(),10000)
 function loading(){
     refres_icon.style.animation = "spin 0.5s linear infinite"
 }
